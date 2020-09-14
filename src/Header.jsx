6dbe3ba05c1f6958e921849b1c5330bb42d64a22
Maxim Frolov <buterbrodhs@gmail.com>
2020-09-14 21:54:29 +0400

    initial project

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./header.css";
+
+export function Header() {
+  return (
+    <header>
+      <h1 className={styles.example}>React-App-Starter</h1>
+    </header>
+  );
+}
