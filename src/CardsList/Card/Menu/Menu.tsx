a12f6e7a1add22cfe51f234559ec0ac249424efa
Maxim Frolov <buterbrodhs@gmail.com>
2020-09-20 20:44:46 +0400

    Добавлено дропдаун меню поста

--- src/CardsList/Card/Menu/Menu.tsx
+++ src/CardsList/Card/Menu/Menu.tsx
@@ -1,22 +1,19 @@
 import React from "react";
 import styles from "./menu.css";
+import { Dropdown } from "../../../Dropdown";
+import { MenuButton } from "./MenuButton";
+import { MenuList } from "./MenuList";
 
 export function Menu() {
   return (
     <div className={styles.menu}>
-      <button className={styles.menuButton}>
-        <svg
-          width="5"
-          height="20"
-          viewBox="0 0 5 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="2.5" cy="2.5" r="2.5" fill="#D9D9D9" />
-          <circle cx="2.5" cy="10" r="2.5" fill="#D9D9D9" />
-          <circle cx="2.5" cy="17.5" r="2.5" fill="#D9D9D9" />
-        </svg>
-      </button>
+      <Dropdown
+        onOpen={() => console.log("opened")}
+        onClose={() => console.log("closed")}
+        button={<MenuButton />}
+      >
+        <MenuList />
+      </Dropdown>
     </div>
   );
 }
---
+++ src/CardsList/Card/Menu/MenuCloseButton/MenuCloseButton.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./menuclosebutton.css";
+
+export function MenuCloseButton() {
+  return <button className={styles.menuCloseButton}>Закрыть</button>;
+}
